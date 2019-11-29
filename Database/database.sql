7707c2f7bc71cf3e7bcb1a40c7ba03011f7aa1d9
Srndo <xsesta06@stud.fit.vutbr.cz>
2019-11-29 17:11:08 +0100

    database update

--- Database/database.sql
+++ Database/database.sql
@@ -2,60 +2,65 @@
 -- ------------------------VYMAZANIE TABS.------------------------------- --
 -- ---------------------------------------------------------------------- --
 
-DROP TABLE PLAN_RIDICE  ;
-DROP TABLE PROVOZNA     ;
-DROP TABLE DENNI_MENU   ;
-DROP TABLE STALA_NABIDKA;
-DROP TABLE NABIDKA      ;
-DROP TABLE POLOZKY      ;
-DROP TABLE OBJEDNAVKA   ;
+DROP TABLE UZIVATEL             ;
+DROP TABLE OPERATOR             ;
+DROP TABLE RIDIC                ;
+DROP TABLE ADMIN                ;
+DROP TABLE STRAVNIK             ;
 DROP TABLE NEREGISTROVANY_UZIVATEL;
-DROP TABLE STRAVNIK     ;
-DROP TABLE ADMIN        ;
-DROP TABLE RIDIC        ;
-DROP TABLE OPERATOR     ;
-DROP TABLE UZIVATEL     ;
+DROP TABLE PLAN_RIDICE          ;
+DROP TABLE OBJEDNAVKA           ;
+DROP TABLE JIDLO                ;
+DROP TABLE PROVOZNA             ;
+DROP TABLE TRVALA_NABIDKA       ;
+DROP TABLE JIDLO_TRVALA_NABIDKA ;
+DROP TABLE DENNI_MENU           ;
+DROP TABLE JIDLO_DENNI_MENU     ;
 
 -- ---------------------------------------------------------------------- --
 -- ------------------------VYTVORENIE TABS.------------------------------ --
 -- ---------------------------------------------------------------------- --
 
 CREATE TABLE UZIVATEL(
-    id          VARCHAR(20) NOT NULL PRIMARY KEY, -- login
-    meno        VARCHAR(20) NOT NULL,
-    priezvisko  VARCHAR(20) NOT NULL,
-    adresa      VARCHAR(20) NOT NULL,
-    tel_cislo   VARCHAR(13) NOT NULL,
-    email       VARCHAR(100) NOT NULL,
-    heslo       VARCHAR(20) NOT NULL
-
-    -- TODO: admin editing UZIVATEL (need model via ALTER TABLE?)
+    id              VARCHAR(10) NOT NULL PRIMARY KEY, -- login
+    meno            VARCHAR(20) NOT NULL,
+    priezvisko      VARCHAR(20) NOT NULL,
+    adresa          VARCHAR(20) NOT NULL,
+    tel_cislo       VARCHAR(13) NOT NULL,
+    email           VARCHAR(100) NOT NULL,
+    heslo           VARCHAR(20) NOT NULL,
+    
+    id_admin_obj    VARCHAR(10) NOT NULL,
+    
+    CONSTRAINT spravuje_uzivatela FOREIGN KEY (id_admin_obj) REFERENCES ADMIN(id)
+    -- NEED TEST ...ADMIN NAMODELOVANY NIZSIE !!! ???
+    -- TOTO NIE JE DOROBENE ANI TO FILLDAT ->
 );
 
 CREATE TABLE OPERATOR(
-    sluzobny_tel    VARCHAR(20) NOT NULL,
-    id              VARCHAR(20) NOT NULL,
+    sluzobny_tel    VARCHAR(13) NOT NULL,
+    id              VARCHAR(10) NOT NULL,
+    
     CONSTRAINT id_operator FOREIGN KEY (id) REFERENCES UZIVATEL(id)
 );
 
 CREATE TABLE RIDIC(
     spz     VARCHAR(10) NOT NULL,
-    id      VARCHAR(20) NOT NULL,
+    id      VARCHAR(10) NOT NULL,
 
     CONSTRAINT id_ridic FOREIGN KEY (id) REFERENCES UZIVATEL(id)
 );
 
-
 CREATE TABLE ADMIN(
     id_ntb     VARCHAR(10) NOT NULL,
-    id         VARCHAR(20) NOT NULL,
+    id         VARCHAR(10) NOT NULL,
 
     CONSTRAINT id_admin FOREIGN KEY (id) REFERENCES UZIVATEL(id)
 );
 
 CREATE TABLE STRAVNIK(
-    limit_jedal INT(10) NOT NULL,
-    id          VARCHAR(20) NOT NULL,
+    cislo_karty     INT(16) NOT NULL, 
+    id              VARCHAR(10) NOT NULL,
 
     CONSTRAINT id_stravnik FOREIGN KEY (id) REFERENCES UZIVATEL(id)
 );
@@ -64,84 +69,96 @@ CREATE TABLE NEREGISTROVANY_UZIVATEL(
     adresa      VARCHAR(20) NOT NULL,
     meno        VARCHAR(20) NOT NULL,
     priezvisko  VARCHAR(20) NOT NULL,
-    tel_cislo   VARCHAR(13)
-);
-  
-CREATE TABLE OBJEDNAVKA(
-	id		            VARCHAR(20) NOT NULL PRIMARY KEY,
-    cena_celkom         INT(5) NOT NULL,
-    stav                VARCHAR(10) NOT NULL,
-    cas_objednania		TIMESTAMP NOT NULL,
-    cas_dorucenia   	TIMESTAMP NOT NULL, -- DATE
-
-    id_operatora    VARCHAR(20),
-    id_stravnika    VARCHAR(20),
-
-    CONSTRAINT ukoncuje_objednavku FOREIGN KEY (id_operatora) REFERENCES OPERATOR(id),
-    CONSTRAINT objednal_objednavku FOREIGN KEY (id_stravnika) REFERENCES STRAVNIK(id)
-);
-
-CREATE TABLE POLOZKY (
-    id      VARCHAR(5) NOT NULL PRIMARY KEY,
-    nazov   VARCHAR(20) NOT NULL,
-    typ     VARCHAR(20) NOT NULL,
-    popis   VARCHAR(100),
-    cena    INT(10) NOT NULL,
-    -- obrázok image
-    
-    id_objednavky VARCHAR(20),
-    id_operator   VARCHAR(20),
-    -- TODO: missing connection to table NABIDKA (N:N)
-    CONSTRAINT id_objednavky_polozky FOREIGN KEY (id_objednavky) REFERENCES OBJEDNAVKA(id),
-    CONSTRAINT id_operator_polozky FOREIGN KEY (id_operator) REFERENCES OPERATOR(id)    
-);
-
-CREATE TABLE NABIDKA(
-    id      VARCHAR(5) NOT NULL PRIMARY KEY
-    -- TODO: missing connection to tables: POLOZKY and STRAVNIK (via ERD)
-);
-
-CREATE TABLE STALA_NABIDKA(
-    hl_jedlo        VARCHAR(20) NOT NULL,
-    priloha         VARCHAR(20) NOT NULL,
-    napoj       VARCHAR(20) NOT NULL,
-    polievka    VARCHAR(20) NOT NULL,
-    id          VARCHAR(5) NOT NULL,
-
-    CONSTRAINT id_stala_nabidka FOREIGN KEY (id) REFERENCES NABIDKA(id)
-);
-
-CREATE TABLE DENNI_MENU(
-    den     VARCHAR(10) NOT NULL,
-    jedlo   VARCHAR(20) NOT NULL,
-    napoj   VARCHAR(20) NOT NULL,
-    polievka VARCHAR(20) NOT NULL,
-    id      VARCHAR(5) NOT NULL,
-
-    CONSTRAINT id_denni_menu FOREIGN KEY (id) REFERENCES NABIDKA(id)
-);
-
-CREATE TABLE PROVOZNA (
-    id          INTEGER(3) NOT NULL PRIMARY KEY,
-    nazov       VARCHAR(100) NOT NULL,
-    adresa      VARCHAR(100) NOT NULL,
-    uzavierka   TIMESTAMP NOT NULL, -- DATETIME ? 
-    
-    id_operatora    VARCHAR(20),
-    id_nabidky      VARCHAR(5),
-
-    CONSTRAINT spravuje_provoznu FOREIGN KEY (id_operatora) REFERENCES OPERATOR(id),
-    CONSTRAINT provozana_ma_nabidku FOREIGN KEY (id_nabidky) REFERENCES NABIDKA(id)
+    tel_cislo   VARCHAR(13) NOT NULL,
 );
 
 CREATE TABLE PLAN_RIDICE(
-    id_regionu      VARCHAR(5) NOT NULL,
-    id_objednavky   VARCHAR(20) NOT NULL,
-    id_ridic        VARCHAR(20) NOT NULL,
-    id_operator     VARCHAR(20) NOT NULL, 
+    id_planu        VARCHAR(10) NOT NULL,
+    region          VARCHAR(10)NOT NULL,
+    id_operatora    VARCHAR(10) NOT NULL,
+    id_ridica       VARCHAR(10) NOT NULL,
 
+    CONSTRAINT id_operatora_plan_ridice FOREIGN KEY (id_operatora) REFERENCES OPERATOR(id),
+    CONSTRAINT id_ridica_plan_ridice FOREIGN KEY (id_ridica) REFERENCES RIDIC(id)
+);  
 
-    CONSTRAINT id_objednavky_plan_ridice FOREIGN KEY (id_objednavky) REFERENCES OBJEDNAVKA(id),
-    CONSTRAINT id_ridice_plan_ridice FOREIGN KEY (id_ridic) REFERENCES RIDIC(id),
-    CONSTRAINT id_operatora_plan_ridice FOREIGN KEY (id_operator) REFERENCES OPERATOR(id)
+CREATE TABLE OBJEDNAVKA(
+    id                  VARCHAR(10) NOT NULL PRIMARY KEY,
+    cena_celkom         INT(10) NOT NULL,
+    stav                VARCHAR(10) NOT NULL,
+    cas_objednania      TIMESTAMP NOT NULL,
+    cas_dorucenia       TIMESTAMP NOT NULL, -- DATE
+
+    id_operatora    VARCHAR(10),
+    id_stravnika    VARCHAR(10),
+    id_plan_ridice  VARCHAR(5),
+    
+    CONSTRAINT ukoncuje_objednavku FOREIGN KEY (id_operatora) REFERENCES OPERATOR(id),
+    CONSTRAINT objednal_objednavku FOREIGN KEY (id_stravnika) REFERENCES STRAVNIK(id),
+    CONSTRAINT obsahuje_objednavku FOREIGN KEY (id_plan_ridice) REFERENCES PLAN_RIDICE(id_planu)
+);
+
+CREATE TABLE JIDLO(
+    id          VARCHAR(5) NOT NULL PRIMARY KEY,
+    nazov       VARCHAR(20) NOT NULL,
+    typ         VARCHAR(20) NOT NULL,
+    popis       VARCHAR(100),
+    alergeny    VARCHAR(20),
+    cena        INT(10) NOT NULL,
+    -- obrázok image
+    
+    id_objednavky   VARCHAR(10),
+    id_operatora    VARCHAR(10),
+   
+    CONSTRAINT id_objednavky_jedla FOREIGN KEY (id_objednavky) REFERENCES OBJEDNAVKA(id),
+    CONSTRAINT id_operator_jedla FOREIGN KEY (id_operatora) REFERENCES OPERATOR(id)    
+);
+
+CREATE TABLE PROVOZNA( 
+    id          INTEGER(3) NOT NULL PRIMARY KEY,
+    nazov       VARCHAR(100) NOT NULL,
+    adresa      VARCHAR(100) NOT NULL,
+    uzavierka   TIMESTAMP NOT NULL,
+    
+    id_operatora    VARCHAR(10),
+
+    CONSTRAINT spravuje_provoznu FOREIGN KEY (id_operatora) REFERENCES OPERATOR(id)
+);
+
+CREATE TABLE TRVALA_NABIDKA(
+    id              VARCHAR(5) NOT NULL,
+    platnost_od     DATE NOT NULL,
+    platnost_do     DATE NOT NULL,
+
+    id_provozny INTEGER(3) NOT NULL,
+
+    CONSTRAINT ponuka_trvala_nabidka FOREIGN KEY (id_provozny) REFERENCES PROVOZNA(id)
+);
+
+CREATE TABLE JIDLO_TRVALA_NABIDKA( -- N:N  
+    jidlo_id            VARCHAR(5) NOT NULL,
+    trvala_nabidka_id   VARCHAR(5) NOT NULL,
+    
+    CONSTRAINT jidlo_trvala_nabidka_jidlo FOREIGN KEY (jidlo_id) REFERENCES JIDLO(id),
+    CONSTRAINT jidlo_trvala_nabidka_tn FOREIGN KEY (trvala_nabidka_id) REFERENCES TRVALA_NABIDKA(id),
+    CONSTRAINT jidlo_trvala_nabidka_unique UNIQUE (jidlo_id, trvala_nabidka_id)    
+);
+
+CREATE TABLE DENNI_MENU(
+    id          VARCHAR(5) NOT NULL,
+    datum       DATE NOT NULL,
+    
+    id_provozny INTEGER(3) NOT NULL,
+    
+    CONSTRAINT ponuka_denni_menu FOREIGN KEY (id_provozny) REFERENCES PROVOZNA(id)
+);
+
+-- **************************************************************************************
+CREATE TABLE JIDLO_DENNI_MENU( -- N:N ••• OBMEDZENIE -> 0 - 5 POCET JEDAL •••
+    jidlo_id            VARCHAR(5) NOT NULL,
+    denne_menu_id       VARCHAR(5) NOT NULL,
+    
+    CONSTRAINT jidlo_denne_menu_jidlo FOREIGN KEY (jidlo_id) REFERENCES JIDLO(id),
+    CONSTRAINT jidlo_denne_menu_dm FOREIGN KEY (denne_menu_id) REFERENCES DENNI_MENU(id),
+    CONSTRAINT jidlo_denne_menu_unique UNIQUE (jidlo_id, denne_menu_id) 
 );
--- Database/fill_data.sql
+++ Database/fill_data.sql
@@ -1,25 +1,27 @@
--- 1/ vzdy prepojeny prvý v riadku s prvým a tak ...
--- 2/ nevyriesena polievka v menu
--- 3/ pri cene €/Kc nemoze byt INTEGER
--- 4/ ID OBJEDNAVKA aspon VARCHAR(10)
--- 5/ upraveny NEREGISTROVANY_UZIVATEL + "priezvisko" (vsade to tak je ->konzistencia DB)
--- 6/ limit jedal by sme mohli nahradit za nieco ako  pocet vernostnych bodov(1 objednavka=1bod) pri STRAVNIKOVI
--- 7/ potrebne telefonne cisla rovnakeho typu 
--- +/ INTEGER asi neberie "+" potrebny VARCHAR(13)  
--- 8/ vyriesenie HESLA 
-------------------------------------------------------------------------------
+/* 
+ 1/ vyriesenie HESLA --> hashovacia funkcia  
 
--- /8
+ 2/ UZIVATEL + id_admin_obj 
+	TO CO JE POUZITE MUSI BYT NAMODELOVANE ... v uzivatelovi modelujeme admina ktory je az po nom namodelovany 
+	--> NEED TEST <--  TEST PRESIEL ...SPRAVNE ?  
+ 
+ 3/ PLAN_RIDICE 1:1 
+
+ 4/ 0..5 JEDAL V MENU 
+
+ 5/ id_regionu ????
+ 
+*/
 
 -- STRAVNICI
-INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo)  
-VALUES('0000123414', 'Leopold', 'Halaj', 'Zahrebska 97', '+421908013019', 'Leopold.Halaj@gmail.com', '???');
-INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo)  
-VALUES('0000259896', 'Izabela', 'Sokolova', 'Vodova 61', '+420786666954', 'Izabela.Sokolova@gmail.com', '???');
-INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo)  
-VALUES('9856745848', 'Julia', 'Cermakova', 'Technicka 1', '+420778695444', 'Julia.Cermakova@gmail.com', '???');
-INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo)  
-VALUES('0012563598', 'David', 'Silber', 'Gorkeho 42', '+421569863457', 'David.Silber@gmail.com', '???');
+INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo, id_admin_obj)  
+VALUES('0000123414', 'Leopold', 'Halaj', 'Zahrebska 97', '+421908013019', 'Leopold.Halaj@gmail.com', '???', '0000025698');
+INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo, id_admin_obj)  
+VALUES('0000259896', 'Izabela', 'Sokolova', 'Vodova 61', '+420786666954', 'Izabela.Sokolova@gmail.com', '???', '0001234569');
+INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo, id_admin_obj)  
+VALUES('9856745848', 'Julia', 'Cermakova', 'Technicka 1', '+420778695444', 'Julia.Cermakova@gmail.com', '???', '0789568236');
+INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo, id_admin_obj)  
+VALUES('0012563598', 'David', 'Silber', 'Gorkeho 42', '+421569863457', 'David.Silber@gmail.com', '???', '5874692354');
 
 -- ADMINI
 INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo)  
@@ -51,7 +53,6 @@ VALUES('0001226598', 'Zora', 'Fererova', 'Masarikova 154', '+420799652114', 'Zor
 INSERT INTO UZIVATEL (id, meno, priezvisko, adresa, tel_cislo, email, heslo)  
 VALUES('0012015486', 'Hugo', 'Lovin', 'Capkova 34', '+420784514598', 'Hugo.Lovin@gmail.com', '???');
 
--- /7
 INSERT INTO OPERATOR (sluzobny_tel, id) 
 VALUES('+420987565369', '0000000048');
 INSERT INTO OPERATOR (sluzobny_tel, id) 
@@ -79,17 +80,15 @@ VALUES('3625956874', '0789568236');
 INSERT INTO ADMIN (id_ntb, id)
 VALUES('1346978526', '5874692354');
 
--- /6
 INSERT INTO STRAVNIK (limit_jedal, id) 
-VALUES('5', '0000123414'); 
+VALUES('4468987566982569', '0000123414'); 
 INSERT INTO STRAVNIK (limit_jedal, id)
-VALUES('6', '0000259896');
+VALUES('4587658932561452', '0000259896');
 INSERT INTO STRAVNIK (limit_jedal, id)
-VALUES('5', '9856745848');
+VALUES('4587652358746589', '9856745848');
 INSERT INTO STRAVNIK (limit_jedal, id)
-VALUES('4', '0012563598');
+VALUES('1245652366665897', '0012563598');
 
--- /5
 INSERT INTO NEREGISTROVANY_UZIVATEL (adresa, meno, priezvisko, tel_cislo) 
 VALUES('Purkynova 16', 'Anastasia', 'Valova', '+421966855744');
 INSERT INTO NEREGISTROVANY_UZIVATEL (adresa, meno, priezvisko, tel_cislo)
@@ -99,82 +98,85 @@ VALUES('Namesti miru 66', 'Tibor', 'Mery', '+420917875489');
 INSERT INTO NEREGISTROVANY_UZIVATEL (adresa, meno, priezvisko, tel_cislo)
 VALUES('Veveri 96', 'Agnesa', 'Varnova', '+420987687487');
 
--- /4
-INSERT INTO OBJEDNAVKA (id, cena_celkom, stav, cas_objednania, cas_dorucenia, id_operatora, id_stravnika) 
-VALUES('1546958732', '35', 'Dorucene', '2017-07-23 14:26:00', '2017-07-23 15:06:00', '0000000048', '0000123414');
-INSERT INTO OBJEDNAVKA (id, cena_celkom, stav, cas_objednania, cas_dorucenia, id_operatora, id_stravnika)
-VALUES('9685743625', '18.6', 'Priprava', '2017-06-23 11:17:00', '2017-06-23 13:17:00', '1526485976', '0000259896');
-INSERT INTO OBJEDNAVKA (id, cena_celkom, stav, cas_objednania, cas_dorucenia, id_operatora, id_stravnika)
-VALUES('9152364859', '3.5', 'Prijata', '2018-02-01 16:34:00', '2018-02-01 17:34:00', '0001226598', '9856745848');
-INSERT INTO OBJEDNAVKA (id, cena_celkom, stav, cas_objednania, cas_dorucenia, id_operatora, id_stravnika)
-VALUES('7589621548', '21.7', 'Dorucene', '2018-03-01 19:47:00', '2018-03-01 20:27:00', '0012015486', '0012563598');
+INSERT INTO PLAN_RIDICE (id_planu, region, id_operator, id_ridica)
+VALUES('5869745812', 'Moravsky region', '0000000048', '2356897845');
+INSERT INTO PLAN_RIDICE (id_planu, region, id_operator, id_ridica)
+VALUES('2585554471', 'Sumava', '1526485976', '0012503654');
+INSERT INTO PLAN_RIDICE (id_planu, region, id_operator, id_ridica)
+VALUES('9966885575', 'Plzensky region', '0001226598', '0000125469');
+INSERT INTO PLAN_RIDICE (id_planu, region, id_operator, id_ridica)
+VALUES('1111171458', 'Prazsky region', '0012015486', '0000001548');
 
--- /3
-INSERT INTO POLOZKY (id, nazov, typ, popis, cena, id_objednavky, id_operator)  
-VALUES('12569', 'Rajec', 'Perliva', 'Napoj bez cukru', '0.80', '1546958732', '0000000048');
-INSERT INTO POLOZKY (id, nazov, typ, popis, cena, id_objednavky, id_operator)
-VALUES('14598', 'Zemiakova kasa', 'BIO', 'Kasa varena z domacich zemiakov s bazalkou', '3', '9685743625', '1526485976');
-INSERT INTO POLOZKY (id, nazov, typ, popis, cena, id_objednavky, id_operator)
-VALUES('24875', 'Cicerova polievka', 'Vegan', 'Strukovinova polievka so zemiakmi a mrkvou', '4', '9152364859', '0001226598');
-INSERT INTO POLOZKY (id, nazov, typ, popis, cena, id_objednavky, id_operator)
-VALUES('33654', 'Ryza', 'Bezlepkova', 'Gulatozrnna ryza s kari korenim', '3.5', '7589621548', '0012015486');
+INSERT INTO OBJEDNAVKA (id, cena_celkom, stav, cas_objednania, cas_dorucenia, id_operatora, id_stravnika, id_plan_ridice) 
+VALUES('1546958732', '35', 'Dorucene', '2017-07-23 14:26:00', '2017-07-23 15:06:00', '0000000048', '0000123414', '5869745812');
+INSERT INTO OBJEDNAVKA (id, cena_celkom, stav, cas_objednania, cas_dorucenia, id_operatora, id_stravnika, id_plan_ridice)
+VALUES('9685743625', '18.6', 'Priprava', '2017-06-23 11:17:00', '2017-06-23 13:17:00', '1526485976', '0000259896', '2585554471');
+INSERT INTO OBJEDNAVKA (id, cena_celkom, stav, cas_objednania, cas_dorucenia, id_operatora, id_stravnika, id_plan_ridice)
+VALUES('9152364859', '3.5', 'Prijata', '2018-02-01 16:34:00', '2018-02-01 17:34:00', '0001226598', '9856745848', '9966885575');
+INSERT INTO OBJEDNAVKA (id, cena_celkom, stav, cas_objednania, cas_dorucenia, id_operatora, id_stravnika, id_plan_ridice)
+VALUES('7589621548', '21.7', 'Dorucene', '2018-03-01 19:47:00', '2018-03-01 20:27:00', '0012015486', '0012563598', '1111171458');
 
--- DENNI_MENU
-INSERT INTO NABIDKA (id)
-VALUES('16498');
-INSERT INTO NABIDKA (id)
-VALUES('12365');
-INSERT INTO NABIDKA (id)
-VALUES('32256');
-INSERT INTO NABIDKA (id)
-VALUES('22215');
+-- TRVALA NABIDKA
+INSERT INTO JIDLO (id, nazov, typ, popis, alergeny, cena, id_objednavky, id_operator)  
+VALUES('12569', 'Rajec', 'napoj', 'Syteny napoj bez cukru', '0', '0.80', '1546958732', '0000000048');
+INSERT INTO JIDLO (id, nazov, typ, popis, alergeny, cena, id_objednavky, id_operator)
+VALUES('14598', 'Zemiakova kasa', 'priloha', 'Kasa varena z domacich zemiakov s bazalkou', '4', '3', '9685743625', '1526485976');
+INSERT INTO JIDLO (id, nazov, typ, popis, alergeny, cena, id_objednavky, id_operator)
+VALUES('24875', 'Cicerova polievka', 'polievka', 'Strukovinova polievka so zemiakmi a mrkvou', '7,9', '4', '9152364859', '0001226598');
+INSERT INTO JIDLO (id, nazov, typ, popis, alergeny, cena, id_objednavky, id_operator)
+VALUES('33654', 'Ryza', 'priloha', 'Gulatozrnna ryza s kari korenim', '10', '3.5', '7589621548', '0012015486');
 
--- STALA_NABIDKA
-INSERT INTO NABIDKA (id)
-VALUES('19998');
-INSERT INTO NABIDKA (id)
-VALUES('16665');
-INSERT INTO NABIDKA (id)
-VALUES('31156');
-INSERT INTO NABIDKA (id)
-VALUES('26815');
+-- MENU 
+INSERT INTO JIDLO (id, nazov, typ, popis, alergeny, cena, id_objednavky, id_operator)   -- neviem ci typ nechat prezdny alebo "MENU"? 
+VALUES('25896', 'Veg Madras, Rajcatova polevka', 'MENU', 'Varena zelenina v stredne palivej omacke a ako priloha Nan', '5,6', '0.80', '1546958732', '0000000048');
+INSERT INTO JIDLO (id, nazov, typ, popis, alergeny, cena, id_objednavky, id_operator)
+VALUES('14785', 'Tatarsky biftek, Zeleninova polevka', 'MENU', 'Mlete hovadzie maso s korenim', '2,4,7', '3', '9685743625', '1526485976');
+INSERT INTO JIDLO (id, nazov, typ, popis, alergeny, cena, id_objednavky, id_operator)
+VALUES('36985', 'Spaghety Bolognese, Cesnekova polevka', 'MENU', 'Spagety s lahodnou omackou', '7,8', '4', '9152364859', '0001226598');
+INSERT INTO JIDLO (id, nazov, typ, popis, alergeny, cena, id_objednavky, id_operator)
+VALUES('55589', 'Pizza Syrova, Brokolicova polevka', 'MENU', 'Pizza s tromi druhmi syra', '1,2,4,10', '3.5', '7589621548', '0012015486');
 
-INSERT INTO STALA_NABIDKA (hl_jedlo, priloha, napoj, polievka, id) 
-VALUES('Vyprazany syr', 'Hranolky', 'Kofola', 'Karfiolova polievka', '19998');
-INSERT INTO STALA_NABIDKA (hl_jedlo, priloha, napoj, polievka, id)
-VALUES('Pecena kacka', 'Opekane zemiaky', 'Rajec', 'Drzkova polievka', '16665');
-INSERT INTO STALA_NABIDKA (hl_jedlo, priloha, napoj, polievka, id)
-VALUES('Rybie file', 'Zemiakova kasa', 'Mattoni', 'Cicerova polievka', '31156');
-INSERT INTO STALA_NABIDKA (hl_jedlo, priloha, napoj, polievka, id)
-VALUES('Grilovane kura', 'Ryza', 'Vinea', 'Kapustnica', '26815');
+INSERT INTO PROVOZNA (id, nazov, adresa, uzavierka, id_operatora)
+VALUES('123', 'Pizzeria Mozarella', 'Kolejni 2 ', '2017-07-23 20:00:00', '0000000048');
+INSERT INTO PROVOZNA (id, nazov, adresa, uzavierka, id_operatora)
+VALUES('255', 'U troch opic', 'Palackeho trida 77', '2017-06-23 19:00:00', '1526485976');
+INSERT INTO PROVOZNA (id, nazov, adresa, uzavierka, id_operatora)
+VALUES('147', 'Everest', 'Grohova 6', '2018-02-01 18:30:00', '0001226598');
+INSERT INTO PROVOZNA (id, nazov, adresa, uzavierka, id_operatora)
+VALUES('012', 'Pivni staj', 'Veveri 38', '2018-03-01 19:30:00', '0012015486');
 
--- /2
-INSERT INTO DENNI_MENU (den, jedlo, napoj, polievka, id) 
-VALUES('Pondelok', 'Veg Madras', 'CocaCola', 'Brokolicovy krem', '16498');
-INSERT INTO DENNI_MENU (den, jedlo, napoj, polievka, id)
-VALUES('Utorok', 'Tatarsky biftek', 'Sprite', 'Paradajkova polievka', '12365');
-INSERT INTO DENNI_MENU (den, jedlo, napoj, polievka, id)
-VALUES('Streda', 'Spaghety Bolognese', 'Bonaqua', 'Gulasova polievka', '32256');
-INSERT INTO DENNI_MENU (den, jedlo, napoj, polievka, id)
-VALUES('Stvrtok', 'Pizza Syrova', 'Tonic', 'Kuraci vyvar', '22215');
+INSERT INTO TRVALA_NABIDKA (id, platnost_od, platnost_do, id_provozny) 
+VALUES('19998', '2019-09-23', '2019-12-20', '123');
+INSERT INTO TRVALA_NABIDKA (id, platnost_od, platnost_do, id_provozny)
+VALUES('16665', '2019-12-21', '2020-03-19', '255');
+INSERT INTO TRVALA_NABIDKA (id, platnost_od, platnost_do, id_provozny)
+VALUES('31156', '2020-03-20', '2020-06-20', '147');
+INSERT INTO TRVALA_NABIDKA (id, platnost_od, platnost_do, id_provozny)
+VALUES('26815', '2020-06-21', '2020-09-22', '012');
 
-INSERT INTO PROVOZNA (id, nazov, adresa, uzavierka, id_operatora, id_nabidky)
-VALUES('123', 'Pizzeria Mozarella', 'Kolejni 2 ', '2017-07-23 20:00:00', '0000000048', '16498');
-INSERT INTO PROVOZNA (id, nazov, adresa, uzavierka, id_operatora, id_nabidky)
-VALUES('255', 'U troch opic', 'Palackeho trida 77', '2017-06-23 19:00:00', '1526485976', '12365');
-INSERT INTO PROVOZNA (id, nazov, adresa, uzavierka, id_operatora, id_nabidky)
-VALUES('147', 'Everest', 'Grohova 6', '2018-02-01 18:30:00', '0001226598', '31156');
-INSERT INTO PROVOZNA (id, nazov, adresa, uzavierka, id_operatora, id_nabidky)
-VALUES('012', 'Pivni staj', 'Veveri 38', '2018-03-01 19:30:00', '0012015486', '26815');
+INSERT INTO JIDLO_TRVALA_NABIDKA (jidlo_id, trvala_nabidka_id) -- prepojenie tabuliek JIDLO a TRVALA_NABIDKA N:N
+VALUES('12569', '19998'); 
+INSERT INTO JIDLO_TRVALA_NABIDKA (jidlo_id, trvala_nabidka_id)
+VALUES('14598', '16665');
+INSERT INTO JIDLO_TRVALA_NABIDKA (jidlo_id, trvala_nabidka_id)
+VALUES('24875', '31156');
+INSERT INTO JIDLO_TRVALA_NABIDKA (jidlo_id, trvala_nabidka_id)
+VALUES('33654', '26815');
 
-INSERT INTO PLAN_RIDICE (id_regionu, id_objednavky, id_ridic, id_operator)
-VALUES('MORAV', '1546958732', '2356897845', '0000000048');
-INSERT INTO PLAN_RIDICE (id_regionu, id_objednavky, id_ridic, id_operator)
-VALUES('SUMAV', '9685743625', '0012503654', '1526485976');
-INSERT INTO PLAN_RIDICE (id_regionu, id_objednavky, id_ridic, id_operator)
-VALUES('PLZEN', '9152364859', '0000125469', '0001226598');
-INSERT INTO PLAN_RIDICE (id_regionu, id_objednavky, id_ridic, id_operator)
-VALUES('PRAHA', '7589621548', '0000001548', '0012015486');
+INSERT INTO DENNI_MENU (id, datum, id_provozny) -- nebude musiet sem ist "ativny datum " ? alebo ako to nazvat ? 
+VALUES('16498', '2019-12-01', '123');
+INSERT INTO DENNI_MENU (id, datum, id_provozny)
+VALUES('12365', '2019-12-02', '255');
+INSERT INTO DENNI_MENU (id, datum, id_provozny)
+VALUES('32256', '2019-12-03', '147');
+INSERT INTO DENNI_MENU (id, datum, id_provozny)
+VALUES('22215', '2019-12-04', '012');
 
-
-
+INSERT INTO JIDLO_DENNI_MENU (jidlo_id, denne_menu_id) -- prepojenie tabuliek JIDLO a DENNI_MENU N:N
+VALUES('25896', '16498'); 
+INSERT INTO JIDLO_DENNI_MENU (jidlo_id, denne_menu_id)
+VALUES('14785', '12365');
+INSERT INTO JIDLO_DENNI_MENU (jidlo_id, denne_menu_id)
+VALUES('36985', '32256');
+INSERT INTO JIDLO_DENNI_MENU (jidlo_id, denne_menu_id)
+VALUES('55589', '22215');
