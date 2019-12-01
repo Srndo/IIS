faf30e51da7ac2778a81a70c189415d7c20f8644
Srndo <xsesta06@stud.fit.vutbr.cz>
2019-12-01 15:49:48 +0100

    .

--- Database/database.sql
+++ Database/database.sql
@@ -1,121 +1,120 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "UZIVATEL" (
-    "id"    INTEGER,
-    "meno"  VARCHAR(20) NOT NULL,
-    "priezvisko"    VARCHAR(20) NOT NULL,
-    "adresa"    VARCHAR(20) NOT NULL,
-    "tel_cislo" VARCHAR(13) NOT NULL,
-    "email" VARCHAR(100) NOT NULL,
-    "heslo" VARCHAR(20) NOT NULL,
-    PRIMARY KEY("id")
+CREATE TABLE IF NOT EXISTS "TRVALA_NABIDKA" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "platnost_od"   VARCHAR(10) NOT NULL,
+    "platnost_do"   VARCHAR(10) NOT NULL,
+    "id_provozny"   INTEGER(3) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "OBJEDNAVKA" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cena_celkom"   INT(10) NOT NULL,
+    "stav"  VARCHAR(10) NOT NULL,
+    "cas_objednania"    VARCHAR(19) NOT NULL,
+    "cas_dorucenia" VARCHAR(19) NOT NULL,
+    "id_operatora"  INTEGER,
+    "id_stravnika"  INTEGER,
+    "id_plan_ridice"    INTEGER
+);
+CREATE TABLE IF NOT EXISTS "PROVOZNA" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nazov" VARCHAR(100) NOT NULL,
+    "adresa"    VARCHAR(100) NOT NULL,
+    "uzavierka" VARCHAR(19) NOT NULL,
+    "id_operatora"  INTEGER
+);
+CREATE TABLE IF NOT EXISTS "DENNI_MENU" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "datum" VARCHAR(10) NOT NULL,
+    "id_provozny"   INTEGER(3) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "ukoncuje_objednavku" (
+    "id"    INTEGER NOT NULL,
+    "id_operatora"  INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "spravuje_provoznu" (
+    "id"    INTEGER NOT NULL,
+    "id_operatora"  INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "ponuka_trvala_nabidka" (
+    "id"    INTEGER NOT NULL,
+    "id_provozny"   INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "ponuka_denni_menu" (
+    "id"    INTEGER NOT NULL,
+    "id_provozny"   INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "obsahuje_objednavku" (
+    "id"    INTEGER NOT NULL,
+    "id_plan_ridice"    INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "objednal_objednavku" (
+    "id"    INTEGER NOT NULL,
+    "id_stravnika"  INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "id_ridica_plan_ridice" (
+    "id_planu"  INTEGER NOT NULL,
+    "id_ridica" INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "id_operator_jedla" (
+    "id"    INTEGER NOT NULL,
+    "id_operatora"  INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "id_objednavky_jedla" (
+    "id"    INTEGER NOT NULL,
+    "id_objednavky" INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS "STRAVNIK" (
     "cislo_karty"   INT(16) NOT NULL,
-    "id"    VARCHAR(10) NOT NULL,
-    FOREIGN KEY("id") REFERENCES "UZIVATEL"("id")
-);
-CREATE TABLE IF NOT EXISTS "OPERATOR" (
-    "sluzobny_tel"  VARCHAR(13) NOT NULL,
-    "id"    VARCHAR(10) NOT NULL,
+    "id"    INTEGER NOT NULL,
     FOREIGN KEY("id") REFERENCES "UZIVATEL"("id")
 );
 CREATE TABLE IF NOT EXISTS "RIDIC" (
     "spz"   VARCHAR(10) NOT NULL,
-    "id"    VARCHAR(10) NOT NULL,
+    "id"    INTEGER NOT NULL,
     FOREIGN KEY("id") REFERENCES "UZIVATEL"("id")
 );
-CREATE TABLE IF NOT EXISTS "ADMIN" (
-    "id_ntb"    VARCHAR(10) NOT NULL,
-    "id"    VARCHAR(10) NOT NULL,
-    FOREIGN KEY("id") REFERENCES "UZIVATEL"("id")
+CREATE TABLE IF NOT EXISTS "PLAN_RIDICE" (
+    "id_planu"  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "region"    VARCHAR(10) NOT NULL,
+    "id_operator"   INTEGER NOT NULL,
+    "id_ridica" INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS "JIDLO" (
-    "id"    VARCHAR(5) NOT NULL,
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "nazov" VARCHAR(20) NOT NULL,
     "typ"   VARCHAR(20) NOT NULL,
     "popis" VARCHAR(100),
     "alergeny"  VARCHAR(20),
     "cena"  INT(10) NOT NULL,
-    "id_objednavky" VARCHAR(10),
-    "id_operator"   VARCHAR(10)
-);
-CREATE TABLE IF NOT EXISTS "PLAN_RIDICE" (
-    "id_planu"  VARCHAR(10) NOT NULL,
-    "region"    VARCHAR(10) NOT NULL,
-    "id_operator"   VARCHAR(10) NOT NULL,
-    "id_ridica" VARCHAR(10) NOT NULL
+    "id_objednavky" INTEGER,
+    "id_operator"   INTEGER
 );
 CREATE TABLE IF NOT EXISTS "JIDLO_DENNI_MENU" (
-    "jidlo_id"  VARCHAR(5) NOT NULL,
-    "denne_menu_id" VARCHAR(5) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "ponuka_denni_menu" (
-    "id"    VARCHAR(5) NOT NULL,
-    "id_provozny"   VARCHAR(3) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "DENNI_MENU" (
-    "id"    VARCHAR(5) NOT NULL,
-    "datum" DATE NOT NULL,
-    "id_provozny"   INTEGER(3) NOT NULL
+    "jidlo_id"  INTEGER NOT NULL,
+    "denne_menu_id" INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS "JIDLO_TRVALA_NABIDKA" (
-    "jidlo_id"  VARCHAR(5) NOT NULL,
-    "trvala_nabidka_id" VARCHAR(5) NOT NULL
+    "jidlo_id"  INTEGER NOT NULL,
+    "trvala_nabidka_id" INTEGER NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "ponuka_trvala_nabidka" (
-    "id"    VARCHAR(5) NOT NULL,
-    "id_provozny"   VARCHAR(3) NOT NULL
+CREATE TABLE IF NOT EXISTS "OPERATOR" (
+    "sluzobny_tel"  VARCHAR(13) NOT NULL,
+    "id"    INTEGER NOT NULL,
+    FOREIGN KEY("id") REFERENCES "UZIVATEL"("id")
 );
-CREATE TABLE IF NOT EXISTS "TRVALA_NABIDKA" (
-    "id"    VARCHAR(5) NOT NULL,
-    "platnost_od"   DATE NOT NULL,
-    "platnost_do"   DATE NOT NULL,
-    "id_provozny"   INTEGER(3) NOT NULL
+CREATE TABLE IF NOT EXISTS "ADMIN" (
+    "id_ntb"    VARCHAR(10) NOT NULL,
+    "id"    INTEGER NOT NULL,
+    FOREIGN KEY("id") REFERENCES "UZIVATEL"("id")
 );
-CREATE TABLE IF NOT EXISTS "spravuje_provoznu" (
-    "id"    VARCHAR(3) NOT NULL,
-    "id_operatora"  VARCHAR(10) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "PROVOZNA" (
-    "id"    INTEGER(3) NOT NULL,
-    "nazov" VARCHAR(100) NOT NULL,
-    "adresa"    VARCHAR(100) NOT NULL,
-    "uzavierka" TIMESTAMP NOT NULL,
-    "id_operatora"  VARCHAR(10)
-);
-CREATE TABLE IF NOT EXISTS "id_operator_jedla" (
-    "id"    VARCHAR(5) NOT NULL,
-    "id_operatora"  VARCHAR(10) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "id_objednavky_jedla" (
-    "id"    VARCHAR(5) NOT NULL,
-    "id_objednavky" VARCHAR(10) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "obsahuje_objednavku" (
-    "id"    VARCHAR(10) NOT NULL,
-    "id_plan_ridice"    VARCHAR(5) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "objednal_objednavku" (
-    "id"    VARCHAR(10) NOT NULL,
-    "id_stravnika"  VARCHAR(10) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "ukoncuje_objednavku" (
-    "id"    VARCHAR(10) NOT NULL,
-    "id_operatora"  VARCHAR(10) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "OBJEDNAVKA" (
-    "id"    VARCHAR(10) NOT NULL,
-    "cena_celkom"   INT(10) NOT NULL,
-    "stav"  VARCHAR(10) NOT NULL,
-    "cas_objednania"    TIMESTAMP NOT NULL,
-    "cas_dorucenia" TIMESTAMP NOT NULL,
-    "id_operatora"  VARCHAR(10),
-    "id_stravnika"  VARCHAR(10),
-    "id_plan_ridice"    VARCHAR(5)
-);
-CREATE TABLE IF NOT EXISTS "id_ridica_plan_ridice" (
-    "id_planu"  VARCHAR(10) NOT NULL,
-    "id_ridica" VARCHAR(10) NOT NULL
+CREATE TABLE IF NOT EXISTS "UZIVATEL" (
+    "id"    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "meno"  VARCHAR(20) NOT NULL,
+    "priezvisko"    VARCHAR(20) NOT NULL,
+    "adresa"    VARCHAR(20) NOT NULL,
+    "tel_cislo" VARCHAR(13) NOT NULL,
+    "email" VARCHAR(100) NOT NULL UNIQUE,
+    "heslo" VARCHAR(20) NOT NULL
 );
 CREATE TABLE IF NOT EXISTS "id_operatora_plan_ridice" (
     "id_planu"  VARCHAR(10) NOT NULL,
--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 import re
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timedelta
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -17,41 +18,26 @@ class Uzivatel(db.Model):
     adresa = db.Column('adresa', db.String(20), nullable = False)
     cislo = db.Column('tel_cislo', db.String(13), nullable = False)
     email = db.Column('email', db.String(100), nullable = False)
-    heslo = db.Column('heslo', db.String(20), nullable = False)
-
-    def __repr__(self):
-        return '<Uzivatel %r>' % self.id
+    heslo = db.Column('heslo', db.Unicode, nullable = False)
 
 class Operator(db.Model):
     sluzobny_tel = db.Column('sluzobny_tel', db.String(13), nullable = False)
     id = db.Column('id', db.String(10), primary_key = True)
-
-    def __repr__(self):
-        return '<Operator %r>' % self.id
 
 
 class Ridic(db.Model):
     spz = db.Column('spz', db.String(10), nullable = False)
     id = db.Column('id', db.String(10), primary_key = True)
 
-    def __repr__(self):
-        return '<Ridic %r>' % self.id
-
 
 class Admin(db.Model):
     id_ntb = db.Column('id_ntb', db.String(10), nullable = False)
     id = db.Column('id', db.String(10), primary_key = True)
 
-    def __repr__(self):
-        return '<Admin %r>' % self.id
-
 
 class Stravnik(db.Model):
     cislo_karty = db.Column('cislo_karty', db.Integer, nullable = False)
     id = db.Column('id', db.String(10), primary_key = True)
-
-    def __repr__(self):
-        return '<Stravnik %r>' % self.id
 
 
 class Plan_ridice(db.Model):
@@ -60,25 +46,16 @@ class Plan_ridice(db.Model):
     id_operatora = db.Column('id_operatora', db.String(10), nullable = False)
     id_ridica = db.Column('id_ridica', db.String(10), nullable = False)
 
-    def __repr__(self):
-        return '<Plan_ridice %r>' % self.id_planu
-
 
 class Objednavka(db.Model):
     id = db.Column('id', db.String(10), primary_key = True)
     cena_celkom = db.Column('cena_celkom', db.Integer, nullable = False)
     stav = db.Column('stav', db.String(10), nullable = False)
-    cas_objednania = db.Column('cas_objednania', db.DateTime, nullable = False)
-    cas_dorucenia = db.Column('cas_dorucenia', db.DateTime, nullable = False)
+    cas_objednania = db.Column('cas_objednania', db.String(16), nullable = False, default=datetime.now)
+    cas_dorucenia = db.Column('cas_dorucenia', db.String(16), nullable = False, default=datetime.now() + timedelta(hours = 1))
     id_operatora = db.Column('id_operatora', db.String(10))
     id_stravnika = db.Column('id_stravnika', db.String(10))
     id_plan_ridice = db.Column('id_plan_ridice', db.String(10))
-    
-    # make own function for getting operator and 'stravnik'
-    # order table have only id's of them
-
-    def __repr__(self):
-        return '<Objednavka %r>' % self.id
 
 
 class Jidlo(db.Model):
@@ -91,56 +68,40 @@ class Jidlo(db.Model):
     id_objednavky = db.Column('id_objednavky', db.String(10), nullable = True)
     id_operator = db.Column('id_operator', db.String(10), nullable = True)
 
-    def __repr__(self):
-        return '<Jidlo %r>' % self.id
-
 
 class Provozna(db.Model):
     id = db.Column('id', db.Integer, primary_key = True)
     nazov = db.Column('nazov', db.String(100), nullable = False)
     adresa = db.Column('adresa', db.String(100), nullable = False)
     uzavierka = db.Column('uzavierka', db.DateTime, nullable = False)
-    #menu = db.relationship("Denni_menu", backref = 'menu_ma_provozna')
-    #nabidka = db.relationship("Trvala_nabidka", backref = 'nabidku_ma_provozna')
-    #id_operatora = db.Column(db.String(10), db.ForeignKey("provozna.id"))
     id_operatora = db.Column('id_operatora', db.String(10), nullable = True)
-
-    def __repr__(self):
-        return '<Provozna %r>' % self.id
 
 
 class Trvala_nabidka(db.Model):
     id = db.Column('id', db.String(5), primary_key = True)
-    platnost_od = db.Column('platnost_od', db.DateTime, nullable = False)
-    platnost_do = db.Column('platnost_do', db.DateTime, nullable = False)
-    #v_provozne = db.Column(db.Integer, db.ForeignKey('provozna.id'))
+    platnost_od = db.Column('platnost_od', db.String(10), nullable = False)
+    platnost_do = db.Column('platnost_do', db.String(10), nullable = False)
     id_provozny = db.Column('id_provozny', db.Integer, nullable = False)
-
-    def __repr__(self):
-        return '<Trvala_nabidka %r>' % self.id
 
 
 class Denni_menu(db.Model):
     id = db.Column('id', db.String(5), primary_key = True)
     datum = db.Column('datum', db.DateTime, nullable = False)
-    #v_provozne = db.Column(db.Integer, db.ForeignKey('provozna.id'))
     id_provozny = db.Column('id_provozny', db.Integer, nullable = False)
-
-    def __repr__(self):
-        return '<Denni_menu %r>' % self.id
 
 
 @app.route('/')
 def index():
     users = Uzivatel.query.order_by(Uzivatel.id).all()
-    return render_template('index.html', users=users)
+    stranici = Stravnik.query.order_by(Stravnik.id).all()
+    return render_template('index.html', users=users, stravnici=stranici)
 
 @app.route('/showSignUp', methods=['POST', 'GET'])
 def singup():
     if request.method == 'POST':
         user_name = request.form['meno']
         user_email = request.form['email']
-        user_password = request.form['heslo']
+        user_password = sha256_crypt.encrypt(request.form['heslo'])
         user_tel = request.form['cislo']
         
         regex = re.compile('^(\+\d{12}|\d{10})$')
@@ -149,18 +110,51 @@ def singup():
         else:
             return "<h2>Zadaj číslo v štýle:</h2><h3>+421999999999<br>090111222333</h3>"
         
-        last_user = Uzivatel.query.order_by(Uzivatel.id.desc()).first()
-        #print(last_user.id)
-        
         new_user = Uzivatel(meno=user_name, priezvisko="Test", adresa="", cislo=user_tel, email=user_email, heslo=user_password)
+        
+        last_user = Uzivatel.query.order_by(Uzivatel.id.desc()).first()
+        new_stravnik = Stravnik(cislo_karty="", id=last_user.id + 1)
+        
         try:
             db.session.add(new_user)
+            db.session.add(new_stravnik)
             db.session.commit()
             return redirect('/')
         except Exception as e:
             return 'There was a issue with adding your task.'
+        
     else:
         return render_template('showSignUp.html')
+
+
+@app.route('/logIn', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        entred_password = request.form['heslo']
+        entred_email = request.form['email']
+        
+        user = Uzivatel.query.filter(Uzivatel.email == entred_email).first()
+
+        if(sha256_crypt.verify(entred_password, user.heslo)):
+            return "<h1>Succesfull</h1>"
+        else:
+            return"<h1>Bad password or email</h1>"
+    else:
+        return render_template('logIn.html')
+
+
+@app.route('/trvalaNabidka')
+def trvalanabidka():
+    provozny = Provozna.query.order_by(Provozna.id).all()
+    nabidky = Trvala_nabidka.query.order_by(Trvala_nabidka.id).all()
+    for nabidka in nabidky:
+        for nabidka2 in nabidky:
+            if (nabidka.id_provozny == nabidka2.id_provozny and nabidka.id != nabidka2.id):
+                print(nabidka.platnost_do + "?" + nabidka2.platnost_od)
+                if nabidka.platnost_do > nabidka2.platnost_od:
+                    return render_template('platnost_error.html', nabidka=nabidka, nabidka2=nabidka2)
+    return render_template('provozny.html', provozny=provozny, nabidky=nabidky)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
