23d835465d0445995d5189a78d9efbdef60ffd16
David Hás <has.david@email.cz>
2019-12-02 12:30:55 +0100

    Create shell script to reinitialize the database

--- init_db.sh
+++ init_db.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
 rm iis.db
-cat ".read Database/database.sql" | sqlite3 iis.db
-cat ".read Database/fill_data.sql" | sqlite3 iis.db
+echo ".read Database/database.sql" | sqlite3 iis.db
+echo ".read Database/fill_data.sql" | sqlite3 iis.db
 
